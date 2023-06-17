5dce1d4611116bdf2bd49ff5ceadc11faad122f9
faiz033033 <feju302@gmail.com>
2023-06-17 09:10:33 +0530

    GettingStartWithReact1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./Component/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Basic Expenses</h2>
+      <h3>Date by Date</h3>
+      <br></br>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,29 @@
+function ExpenseItem () {
+    return (
+        <div>
+            <div>March 2021</div>
+            <div>
+                <h2>Food Item</h2>
+                <div>Rs 10</div>
+            </div>
+            --------------------------------
+            <br></br>
+        
+            <div>April 2021</div>
+            <div>
+                <h2>Petrol</h2>
+                <div>Rs 100</div>
+            </div>
+            ---------------------------------
+            <br></br>
+            <div>April 2021</div>
+            <div>
+                <h2>Movies</h2>
+                <div>Rs 200</div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default ExpenseItem;
