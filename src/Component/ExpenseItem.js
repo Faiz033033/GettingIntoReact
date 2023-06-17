66dd5b04febe9234591ed15ba9b339a223a728a2
faiz033033 <feju302@gmail.com>
2023-06-17 19:30:29 +0530

    MakingCompSmaller_AddingDateStyle

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css';
+import './ExpenseDate.js';
 
 function ExpenseItem (props) {
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date} />
             <div>{props.location}</div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>Rs{props.amount}</div>
             </div>
-        </div>
-        
+        </div>  
     );
 }
 
