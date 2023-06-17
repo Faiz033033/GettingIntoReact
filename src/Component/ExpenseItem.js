52abc16b99e87ff886676a510c7125ac9cc527d1
faiz033033 <feju302@gmail.com>
2023-06-17 10:11:32 +0530

    AddedLocationVariable

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -4,9 +4,11 @@ function ExpenseItem () {
     const expenseDate = new Date(2021,2,28);
     const expenseTitle = 'Car Insure';
     const expenseAmount = 2000
+    const expenselocation = 'Jaipur';
     return (
         <div className='expense-item'>
             <div>{expenseDate.toISOString()}</div>
+            <div>{expenselocation}</div>
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>Rs{expenseAmount}</div>
