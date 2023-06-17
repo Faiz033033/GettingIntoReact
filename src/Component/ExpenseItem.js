ec9c29b5b8eba56649bff135ec9c31e407041810
faiz033033 <feju302@gmail.com>
2023-06-17 19:36:41 +0530

    ErrorResolved

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,17 +1,17 @@
 import './ExpenseItem.css';
-import './ExpenseDate.js';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem (props) {
-    return (
-        <div className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div>{props.location}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>Rs{props.amount}</div>
-            </div>
-        </div>  
-    );
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div>{props.location}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>Rs{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
