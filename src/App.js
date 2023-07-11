53cd75f49f3710ad9a6cd29a40e167603789917a
faiz033033 <feju302@gmail.com>
2023-07-11 22:15:20 +0530

    AddandCancelWorking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
@@ -26,7 +27,6 @@ const DUMMY_EXPENSES = [
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  const [showForm, setShowForm] = useState(false);
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
@@ -34,24 +34,18 @@ const App = () => {
     });
   };
 
-  const showFormHandler = () => {
-    setShowForm(true);
-  };
-
-  const hideFormHandler = () => {
-    setShowForm(false);
-  };
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
-      {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
-      {showForm && (
-        <NewExpense
-          onAddExpense={addExpenseHandler}
-          onHideForm={hideFormHandler}
-        />
-      )}
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
+      
     </div>
   );
 };
